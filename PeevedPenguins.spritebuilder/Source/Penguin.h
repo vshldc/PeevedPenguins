ac6e5083c3753913663a1e4e0e745f213e832092
Vishal <vshldc@gmail.com>
2015-02-03 02:17:56 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Vishal Chandnani on 2/3/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,21 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Vishal Chandnani on 2/3/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+@end
