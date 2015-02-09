8ed061554d09f7499df2cb06d4e1d8dd564a555b
Vishal <vshldc@gmail.com>
2015-02-09 04:54:41 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -15,6 +15,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_gamePlayScrollingNode;
+    CCNode *_contentNode;
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
