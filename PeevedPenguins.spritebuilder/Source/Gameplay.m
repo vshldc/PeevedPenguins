b44a407364514ec5d161996c3c784d7e73ffff65
Vishal <vshldc@gmail.com>
2015-02-11 06:07:51 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -33,8 +33,6 @@ static const float MIN_SPEED = 7.f;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
     
-    // visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
     
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
